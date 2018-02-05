8dfd52a5daaf4b6c1265482ea47c3029a2f59935
memsharded <james@conan.io>
2018-02-05 13:47:30 +0100

    update to conan 1.0

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -2,19 +2,13 @@ from conans import ConanFile, CMake
 import os
 
 
-channel = os.getenv("CONAN_CHANNEL", "testing")
-username = os.getenv("CONAN_USERNAME", "memsharded")
-
-
 class HelloTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "Hello/0.1@%s/%s" % (username, channel)
     generators = "cmake"
 
     def build(self):
         cmake = CMake(self)
-        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
-        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.configure()
         cmake.build()
 
     def imports(self):
